e4bd7f8fcc80038f1e952ee5bb44e70ffb8340aa
appanikaushik <130453347+AyushiShukla1703@users.noreply.github.com>
2024-03-26 23:33:42 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios"
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+
+  const [users, setUsers] = useState([]); 
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+
+  const loadData = async () => {
+    const response = await Axios.get('http://localhost:3003/users');
+    console.log(response.data);
+    setUsers(response.data)
+  }
+
+  const AddUser = (e) => {
+    e.preventDefault();
+    Axios.post('http://localhost:3003/users', {
+      id, name
+    }).then(() => {
+      setId(""); setName("");
+    }).catch((err) => {
+      console.log(err);
+    })
+    setTimeout(() => {
+      loadData();
+    }, 500)
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input placeholder='Enter ID ..' value={id} onChange={e => setId(e.target.value)} />
+      <input placeholder='Enter Name ..' value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={AddUser}>Add</button>
+
+      {
+        users.map(e => (
+          <div>
+            {e.id} {e.name}
+          </div>
+        ))
+      }
     </div>
   );
 }
